dfa1821c81f1316d207ca5eeecc950229f79964e
Thijs W <mawijnen2@student.avans.nl>
2019-09-10 12:09:14 +0200

    added challenge 2

--- CryptopalsSetOne/CryptopalsSetOne/Program.cs
+++ CryptopalsSetOne/CryptopalsSetOne/Program.cs
@@ -20,11 +20,28 @@ namespace CryptopalsSetOne
         {
             string result = HexString2B64String("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
             if (result != "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t") Console.WriteLine("something went wrong");
-            else Console.WriteLine("challenge 1 result: " + result);
+            else Console.WriteLine("Challenge 1 result: " + result);
         }
         private static void challengeTwo()
         {
+            byte[] input = HexStringToHex("1c0111001f010100061a024b53535009181c");
+            byte[] xorKey = HexStringToHex("686974207468652062756c6c277320657965");
+            byte[] result = XorEqualBuffers(input, xorKey);
+            string resultString = ByteArrayToString(result);
+            if (resultString != "746865206b696420646f6e277420706c6179".ToUpper()) Console.WriteLine("something went wrong");
+            else Console.WriteLine("Challenge 2 result:\n\thex: " + resultString + "\n\tascii: " + Encoding.Default.GetString(result));
             
+        }
+
+        public static byte[] XorEqualBuffers(byte[] input, byte[] xorKey)
+        {
+            byte[] result = new byte[input.Length];
+            for (var i = 0; i < result.Length; i++)
+            {
+                result[i] = (byte) (input[i] ^ xorKey[i]);
+            }
+
+            return result;
         }
 
         public static string HexString2B64String(string input)
@@ -44,6 +61,11 @@ namespace CryptopalsSetOne
                 resultantArray[i] = Convert.ToByte(inputHex.Substring(i * 2, 2), 16);
             }
             return resultantArray;
+        }
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            return BitConverter.ToString(ba).Replace("-", "");
         }
     }
 }
